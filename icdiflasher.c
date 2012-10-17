8c8d73bd091d9508e648a07a17882f26b1a2d04b
Fabio Utzig <fabio@utzig.net>
2012-10-17 20:42:02 -0300

    Add license

--- icdiflasher.c
+++ icdiflasher.c
@@ -1,3 +1,21 @@
+/* icdiflasher - TI Stellaris Launchpad compatible ICDI flasher
+ * Copyright (C) 2012 Fabio Utzig
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -5,12 +23,11 @@
 
 #include <fcntl.h>
 #include <sys/types.h>
-//#include <sys/uio.h>
 #include <unistd.h>
 
 #include <libusb-1.0/libusb.h>
 
-#define DEBUG 1
+//#define DEBUG 1
 
 // Flash Patch and Breakpoint: see ARM Debug Interface V5 Architecture Specification
 static const uint32_t FPB     = 0xe0002000;
